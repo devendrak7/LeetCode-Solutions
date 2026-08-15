914c3fa4b8987a7bded92fd32692c8052c336f44
Devendra Kumawat <devendrak9610@gmail.com>
2026-08-15 16:24:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newS;
+        // string newS;
         
-        for(char ch : s){
-            if(isalnum(ch)){
-                ch = tolower(ch);
-                newS+=ch;
+        // for(char ch : s){
+        //     if(isalnum(ch)){
+        //         ch = tolower(ch);
+        //         newS+=ch;
+        //     }
+        // }
+        // int n = newS.size();
+        // for(int i = 0;i<n/2;i++){
+        //     if(newS[i]!=newS[n-1-i]){
+        //         return false;
+        //     }
+
+        // }
+        int left = 0;
+        int right = s.size()-1;
+        while(left<right){
+            while(left<right&&!isalnum(s[left])){
+                left++;
             }
-        }
-        int n = newS.size();
-        for(int i = 0;i<n/2;i++){
-            if(newS[i]!=newS[n-1-i]){
+            while(left<right&&!isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left])!=tolower(s[right])){
                 return false;
             }
-
+            left++;
+            right--;
         }
         return true ;
         }
